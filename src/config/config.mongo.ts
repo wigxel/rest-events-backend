09d93a0bf2efbdfdedffbde4961142aa317a7267
Joseph Julius <Joseph.owonwo@gmail.com>
2019-11-17 17:45:58 +0100

    refactor: changed the log message

--- src/config/config.mongo.ts
+++ src/config/config.mongo.ts
@@ -2,21 +2,23 @@ import { mongoose } from '@typegoose/typegoose';
 
 import { getDbUrl, isProduction } from './config.helper';
 
+if (!isProduction()) {
+  mongoose.set("debug", true);
+}
+
 export const mongooseConnection = async () => {
-  let mongooseConnection = null;
   try {
-    mongooseConnection = await mongoose.connect(getDbUrl('mongodb'), {
+    const connection = await mongoose.connect(getDbUrl('mongodb'), {
       dbName: process.env.MONGODB_DATABASE_NAME,
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-
-    !isProduction() && mongoose.set('debug', true);
-
-    console.log('>>> successfully connected to mongodb \n');
+    console.log(">> Successfully connected to MongoDB \n");
+    
+    return connection;
   } catch (err) {
-    console.error('>>> Failed to connect mongodb \n', err);
-  } finally {
-    return mongooseConnection;
+    console.error('>> Failed to connect MongoDB \n', err);
   }
+
+  return null;
 };
--- src/config/config.typeorm.ts
+++ src/config/config.typeorm.ts
@@ -3,14 +3,13 @@ import { createConnection } from 'typeorm';
 import config from '../../ormconfig';
 
 export const typeormConnection = async () => {
-  let typeormConnection = null;
   try {
-    typeormConnection = await createConnection(config);
-
-    console.log('>>> successfully connected to postgres \n');
+    const connection = await createConnection(config);
+    console.log('>> Successfully connected to Postgres \n');
+    return connection;
   } catch (err) {
-    console.error('>>> Failed to connect postgres \n', err);
-  } finally {
-    return typeormConnection;
+    console.error('>> Failed to connect Postgres \n', err);
   }
+
+  return null;
 };
